f24787b4fd23551fb4fc25b254f8dc46321e831f
GyeongjunHa <32204781@dankook.ac.kr>
2024-07-26 00:14:33 +0900

    feat: Add RateDiscountPolicy and RateDiscountPolicyTest

---
+++ src/main/java/hello/core_self_practice/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core_self_practice.discount;
+
+import hello.core_self_practice.member.Grade;
+import hello.core_self_practice.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+
+}
--- src/main/java/hello/core_self_practice/order/OrderServiceImpl.java
+++ src/main/java/hello/core_self_practice/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core_self_practice.order;
 
 import hello.core_self_practice.discount.DiscountPolicy;
 import hello.core_self_practice.discount.FixDiscountPolicy;
+import hello.core_self_practice.discount.RateDiscountPolicy;
 import hello.core_self_practice.member.Member;
 import hello.core_self_practice.member.MemberRepository;
 import hello.core_self_practice.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import hello.core_self_practice.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+     private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
