2367127eff508fbf473787bcb5622c507cab7b83
GyeongjunHa <32204781@dankook.ac.kr>
2024-07-26 13:38:45 +0900

    refactor: Changed the code remove duplication and reveal the role(interface) better than before.

--- src/main/java/hello/core_self_practice/AppConfig.java
+++ src/main/java/hello/core_self_practice/AppConfig.java
@@ -10,14 +10,22 @@ import hello.core_self_practice.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
+    }
+
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
