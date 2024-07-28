1ff05bfe69af0bf0f0367af1b7dcf01076b32a10
GyeongjunHa <32204781@dankook.ac.kr>
2024-07-28 18:32:09 +0900

    test: Add ConfigurationSingletonTest and its test method

--- src/main/java/hello/core_self_practice/AppConfig.java
+++ src/main/java/hello/core_self_practice/AppConfig.java
@@ -14,11 +14,15 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        //1번
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public OrderService orderService() {
+        //1번
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy()
@@ -27,6 +31,8 @@ public class AppConfig {
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        //2번? 3번?
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
