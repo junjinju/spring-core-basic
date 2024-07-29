977610435100e10bbf44f80e3d077206216c50b6
GyeongjunHa <32204781@dankook.ac.kr>
2024-07-29 15:46:12 +0900

    test: Auto-register bean vs Non-auto-register bean

--- src/main/java/hello/core_self_practice/AutoAppConfig.java
+++ src/main/java/hello/core_self_practice/AutoAppConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-import static org.springframework.context.annotation.ComponentScan.*;
+import static org.springframework.context.annotation.ComponentScan.Filter;
 
 @Configuration
 @ComponentScan(
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+
 }
