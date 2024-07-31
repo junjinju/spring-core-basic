9ef320d743222927b3d9bdfe0ea61a5fa6b65ee3
GyeongjunHa <32204781@dankook.ac.kr>
2024-08-01 06:10:34 +0900

    test: change MyLogger to ObjectProvider: LogDemoController, and LogDemoService

--- src/main/java/hello/core_self_practice/web/LogDemoService.java
+++ src/main/java/hello/core_self_practice/web/LogDemoService.java
@@ -2,15 +2,17 @@ package hello.core_self_practice.web;
 
 import hello.core_self_practice.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 
