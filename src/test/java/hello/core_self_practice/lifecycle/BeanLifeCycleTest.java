27a4d79ee74eb60a5ead79e071c8d8f0984b4228
GyeongjunHa <32204781@dankook.ac.kr>
2024-08-01 00:46:41 +0900

    test: implements InitializingBean and DisposableBean

---
+++ src/test/java/hello/core_self_practice/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,36 @@
+package hello.core_self_practice.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+
+        NetworkClient client = ac.getBean(NetworkClient.class);
+
+        /*
+        스프링 컨테이너를 종료, ApplicationContext -> ConfigurableApplicationContext -> Annotation...
+        ac.close() 호출을 위해 ConfigurableApplicationContext 필요
+         */
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+
+    }
+
+}
